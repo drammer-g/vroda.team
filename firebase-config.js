c5df1306ffbdd96c6c79beff9bff44a727c799dc
drammer_g <drammer.g@gmail.com>
2022-10-10 16:47:06 +0300

    test auth page

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBOTU2IOyB8KIgAdhln2lHj61AjU-szTcY",
+    authDomain: "vroda-team-f1f92.firebaseapp.com",
+    databaseURL: "https://vroda-team-f1f92-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "vroda-team-f1f92",
+    storageBucket: "vroda-team-f1f92.appspot.com",
+    messagingSenderId: "966153564333",
+    appId: "1:966153564333:web:7ad6414bfa2edc75cb1624",
+    measurementId: "G-NGPFV9C8CL"
+};
+
+export default firebaseConfig;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,128 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <button @click="makeUser">Hello from development 2</button>
+    <h1 v-if="getUserName">hello {{ getUserName }} </h1>
+    <button @click="signIn">Sign in</button>
+    <button @click="signOutUser">Exit</button>
+    <button @click="saveMessage">Save Message</button>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
+import {initializeApp} from 'firebase/app';
+// import {
+//   getAuth,
+//   onAuthStateChanged,
+//   GoogleAuthProvider,
+//   signInWithPopup,
+//   signOut,
+// } from 'firebase/auth';
+
+import {
+  getAuth,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
+// import {
+//   getFirestore,
+//   collection,
+//   addDoc,
+//   query,
+//   orderBy,
+//   limit,
+//   onSnapshot,
+//   setDoc,
+//   updateDoc,
+//   doc,
+//   serverTimestamp,
+// } from 'firebase/firestore';
+//
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  serverTimestamp,
+} from 'firebase/firestore';
+
+// import {
+//   getStorage,
+//   ref,
+//   uploadBytesResumable,
+//   getDownloadURL,
+// } from 'firebase/storage';
+// import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+// import { getPerformance } from 'firebase/performance';
+
+import firebaseConfig from "../../firebase-config";
+// Import the functions you need from the SDKs you need
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
   },
+  data() {
+    return {
+      currentUser: null
+    }
+  },
+  computed: {
+    // Returns the signed-in user's display name.
+    getUserName: {
+      get() {
+        return (!!getAuth()?.currentUser) ? this.currentUser.displayName : null;
+      }
+    },
+  },
   methods: {
-    makeUser () {
-
+    // Initialize firebase auth
+    initFirebaseAuth() {
+      // Listen to auth state changes.
+      onAuthStateChanged(getAuth(), authStateObserver);
+    },
+    signOutUser() {
+      signOut(getAuth());
+    },
+    isUserSignedIn() {
+      return !!getAuth().currentUser;
+    },
+// Signs-in Friendly Chat.
+    async signIn() {
+      const app = this;
+      // Sign in Firebase using popup auth and Google as the identity provider.
+      let provider = new GoogleAuthProvider();
+      await signInWithPopup(getAuth(), provider).then((result) => {
+        app.currentUser = result.user;
+      })
+    },
+    // Saves a new message to Cloud Firestore.
+    async saveMessage(messageText) {
+      // Add a new message entry to the Firebase database.
+      console.log({
+        name: this.getUserName,
+        text: "messageText test",
+        profilePicUrl: null,
+        timestamp: serverTimestamp()
+      })
+      try {
+        await addDoc(collection(getFirestore(), 'messages'), {
+          name: this.getUserName,
+          text: "messageText test",
+          profilePicUrl: null,
+          timestamp: serverTimestamp()
+        });
+      } catch (error) {
+        console.error('Error writing new message to Firebase Database', error);
+      }
     }
   }
 }
@@ -24,14 +133,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
